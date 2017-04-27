5e36755f1715c7a4f58c83c0611041ddb8cdc3df
Martin Kowaleff <martinkowaleff@martins-mbp.cable.rcn.com>
2017-04-27 11:44:15 -0400

    Initial Commit

---
+++ Add 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Add 1
+//
+//  Created by Martin Kowaleff on 4/27/17.
+//  Copyright © 2017 Martin Kowaleff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
